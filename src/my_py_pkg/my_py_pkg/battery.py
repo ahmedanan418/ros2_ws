4953ebb4117f712e9715b73f46590549e63c4178
ahmedanan418 <ahmed.anan418@gmail.com>
2024-09-03 15:58:42 +0300

    battery update

--- src/my_py_pkg/my_py_pkg/battery.py
+++ src/my_py_pkg/my_py_pkg/battery.py
@@ -7,6 +7,26 @@ class BatteryNode(Node): #Change MyNode
     def __init__(self):
         super().__init__("battery") 
         self.battery_state="full"
+        self.last_time_battery_has_changed= self.get_current_time_seconds()
+        self.battery_timer=self.create_timer(0.1,self.check_battery_state)
+
+    def get_current_time_seconds(self):
+        secs, nsecs= self.get_clock().now().seconds_nanoseconds()
+        return secs+nsecs/1000000000.0
+
+    def check_battery_state(self):
+        time_now= self.get_current_time_seconds()
+        if self.battery_state == "full":
+            if time_now-self.last_time_battery_has_changed>4.0:
+                self.battery_state="empty"
+                self.get_logger().info("Battery is empty! Charging ...")
+                self.last_time_battery_has_changed=time_now
+        else:
+            if time_now-self.last_time_battery_has_changed>6.0:
+                self.battery_state="full"
+                self.get_logger().info("Battery charged successfully")
+                self.last_time_battery_has_changed=time_now
+
     
 #This main function will always be the same 
 def main(args=None):
