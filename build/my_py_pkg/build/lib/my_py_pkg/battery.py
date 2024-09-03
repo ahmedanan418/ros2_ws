a5b290296946277f8dba8158cb025197a599bd25
ahmedanan418 <ahmed.anan418@gmail.com>
2024-09-03 17:52:47 +0300

    Some files after colcon build. Need to know how to git ignore

--- build/my_py_pkg/build/lib/my_py_pkg/battery.py
+++ build/my_py_pkg/build/lib/my_py_pkg/battery.py
@@ -1,31 +1,57 @@
 #!/usr/bin/env python3
 import rclpy
 from rclpy.node import Node
+from functools import partial
+
+from my_robot_interfaces.srv import SetLed
 
 #Write your code inside this class always
-class BatteryNode(Node): #Change MyNode 
+class BatteryNode(Node):
     def __init__(self):
-        super().__init__("battery") 
-        self.battery_state="full"
-        self.last_time_battery_has_changed= self.get_current_time_seconds()
-        self.battery_timer=self.create_timer(0.1,self.check_battery_state)
+        super().__init__("battery")
+        self.battery_state_ = "full"
+        self.last_time_battery_state_changed_ = self.get_current_time_seconds()
+        self.battery_timer_ = self.create_timer(0.1, self.check_battery_state)
+        self.get_logger().info("Battery node has been started")
 
     def get_current_time_seconds(self):
-        secs, nsecs= self.get_clock().now().seconds_nanoseconds()
-        return secs+nsecs/1000000000.0
+        secs, nsecs = self.get_clock().now().seconds_nanoseconds()
+        return secs + nsecs / 1000000000.0
 
     def check_battery_state(self):
-        time_now= self.get_current_time_seconds()
-        if self.battery_state == "full":
-            if time_now-self.last_time_battery_has_changed>4.0:
-                self.battery_state="empty"
-                self.get_logger().info("Battery is empty! Charging ...")
-                self.last_time_battery_has_changed=time_now
+        time_now = self.get_current_time_seconds()
+        if self.battery_state_ == "full":
+            if time_now - self.last_time_battery_state_changed_ > 4.0:
+                self.battery_state_ = "empty"
+                self.get_logger().info("Battery is empty! Charging battery...")
+                self.last_time_battery_state_changed_ = time_now
+                self.call_set_led_server(3, 1)
         else:
-            if time_now-self.last_time_battery_has_changed>6.0:
-                self.battery_state="full"
-                self.get_logger().info("Battery charged successfully")
-                self.last_time_battery_has_changed=time_now
+            if time_now - self.last_time_battery_state_changed_ > 6.0:
+                self.battery_state_ = "full"
+                self.get_logger().info("Battery is now full again.")
+                self.last_time_battery_state_changed_ = time_now
+                self.call_set_led_server(3, 0)
+
+    def call_set_led_server(self, led_number, state):
+        client = self.create_client(SetLed, "set_led")
+        while not client.wait_for_service(1.0):
+            self.get_logger().warn("Waiting for Server Add Two Ints...")
+
+        request = SetLed.Request()
+        request.led_number = led_number
+        request.state = state
+
+        future = client.call_async(request)
+        future.add_done_callback(
+            partial(self.callback_call_set_led, led_number=led_number, state=state))
+
+    def callback_call_set_led(self, future, led_number, state):
+        try:
+            response = future.result()
+            self.get_logger().info(str(response.success))
+        except Exception as e:
+            self.get_logger().error("Service call failed %r" % (e,))
 
     
 #This main function will always be the same 
